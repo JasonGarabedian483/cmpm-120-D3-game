236388e11e205a6ed19d9fc99e278b44b4ee1cf8
JasonGarabedian483 <jrgarabe@ucsc.edu>
2026-05-07 01:14:52 -0700

    added block with collision and movement based on character hitting

--- game.js
+++ game.js
@@ -6,18 +6,42 @@ class levelOne extends Phaser.Scene {
         this.load.path = 'assets/'
         this.load.image('character', 'char.png');
         this.load.image('ground', 'ground.png');
+        this.load.image('brick', 'brick.png');
 
 
     }
     create() {
         const graphics = this.add.graphics({ lineStyle: { width: 10, color: 0xffffff, alpha: 0.5 } });
-        const line = new Phaser.Geom.Line();
-        const ground = this.add.image(1920/2, 1000, 'ground').setScale(2.5);
-            ground.setInteractive({useHandCursor: true});
-        const char1 = this.add.sprite(300, 850, 'character').setScale(.4);
-        const char1P = this.physics.add.sprite(300, 850, 'character').setScale(.4); // create character
-            char1P.disableBody(true, true);
+        let line = new Phaser.Geom.Line();
+        let ground = this.physics.add.staticImage(1920/2, 1000, 'ground')
+            .setScale(2.5)
+            .setInteractive({useHandCursor: true})
+            .refreshBody();
+        const char1 = this.add.sprite(300, 850, 'character')
+            .setScale(.4);
+        const char1P = this.physics.add.sprite(300, 850, 'character')
+            .setScale(.4)
+            .disableBody(true, true)
+            .setDrag(.9, 0)
+            .setDamping(true);
+        const blocks = this.physics.add.group({
+            defaultKey: 'brick',
+            bounceX: 1,
+            bounceY: 1,
+            collideWorldBounds: true,
+            dragX: 0.5,
+            dragY: 0.5,
+            useDamping: true
+        });
 
+        const block = blocks.create(1000, 835)
+            .setMass(2)
+            .setScale(8);
+        
+        char1P.setCollideWorldBounds(true) // eventually set to false so player can miss
+        this.physics.add.collider(char1P, ground);
+        this.physics.add.collider(ground, block);
+        this.physics.add.collider(char1P, block);
         let angle = 0
 
         this.input.on('pointermove', (pointer) => {
@@ -30,7 +54,20 @@ class levelOne extends Phaser.Scene {
         this.input.on('pointerup', () => {
             char1P.enableBody(true, 300, 850, true, true);
             this.physics.velocityFromRotation(angle, 600, char1P.body.velocity);
+             /*this.tweens.add({
+                targets: [char1], 
+                alpha: 0,
+                duration: 0
+            }); */
         });
+
+        this.input.keyboard.on('keydown-R', () => {
+            this.scene.restart();
+        });
+
+    }
+
+    update() {
     }
 }
 
