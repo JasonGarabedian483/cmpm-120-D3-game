14b7f7667003d481f4a40b67c69aad042c53a5a5
JasonGarabedian483 <jrgarabe@ucsc.edu>
2026-05-08 16:06:59 -0700

    started development of level 2, along with updating readme

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ ANGRY GOO
         - Maybe just make him jump? use arrow keys to choose trajectory then spacebar to launch
     - Finish summary screens
     - Finish levelTwo and start levelThree
+    - Possibly change background to a drawn image
+    - Maybe color code walls based on how it increase velocity
--- game.js
+++ game.js
@@ -86,10 +86,10 @@ class levelOne extends Phaser.Scene {
 
         // Creating physics collisions
         this.physics.add.collider(char1P, ground);
-        this.physics.add.collider(ground, block);
-        this.physics.add.collider(char1P, block);
+        this.physics.add.collider(ground, blocks);
+        this.physics.add.collider(char1P, blocks);
         this.physics.add.collider(char1P, walls);
-        this.physics.add.collider(block, walls);
+        this.physics.add.collider(blocks, walls);
         this.physics.add.overlap(char1P, coins, (char1P, coin) => {
                 coin.destroy();
                 this.score += 1;
@@ -146,11 +146,133 @@ class levelTwo extends Phaser.Scene{ // FINISH THIS
         super('leveltwo')
     }
     preload() {
+        this.load.path = 'assets/'
+        this.load.image('character', 'char.png');
+        this.load.image('ground', 'ground.png');
+        this.load.image('brick', 'brick.png');
+        this.load.image('coin', 'coin.png');
 
+
+    }
+    create() {
+        // Set background color
+        this.cameras.main.setBackgroundColor('#67BED9');
+        // Declaring base variables
+        let angle = 0
+        this.score = 0
+        this.lives = 3
+
+        // Score / Lives texts
+        this.livesText = this.add.text(100, 100, 'Lives: ' + this.lives, {fontSize: '32px', color: '#000000'});
+        this.scoreText = this.add.text(100, 150, 'Score: ' + this.score, {fontSize: '32px', color: '#000000'});
+
+        // Creating assets / asset groups
+        const graphics = this.add.graphics({
+            lineStyle: {
+                width: 10,
+                color: 0xffffff,
+                alpha: 0.5
+            }});
+        let line = new Phaser.Geom.Line();
+        let ground = this.physics.add.staticImage(1920/2, 1000, 'ground')
+            .setScale(2.5)
+            .setInteractive({useHandCursor: true})
+            .refreshBody();
+        const char1 = this.add.sprite(300, 850, 'character')
+            .setScale(.4);
+        const char1P = this.physics.add.sprite(300, 850, 'character')
+            .setScale(.4)
+            .setDepth(1)
+            .disableBody(true, true)
+            .setDrag(.9, 0)
+            .setDamping(true)
+            .setBounce(.25, .6)
+            //.setCollideWorldBounds(true);
+
+        // Function to create blocks: use blocks.create(x, y)
+        const blocks = this.physics.add.group({
+            defaultKey: 'brick',
+            bounceX: 1,
+            bounceY: 1,
+            collideWorldBounds: true,
+            dragX: 0.5,
+            dragY: 0.5,
+            useDamping: true
+        });
+
+        // Function to create walls: use createWall(x, y, width, height)
+        const walls = this.physics.add.staticGroup();
+        const createWall = (x, y, width, height, color = 0xffffff) => {
+            const wall = this.add.rectangle(x, y, width, height, color)
+            this.physics.add.existing(wall, true);
+            walls.add(wall);
+            return wall;
+        };
+
+        // Function to create coins: use createCoin(x, y)
+        const coins = this.physics.add.group();
+        const createCoin = (x, y) => {
+            const coin = coins.create(x, y, 'coin');
+            coin.setScale(4);
+            coin.setDepth(0);
+            coin.body.allowGravity = false;
+            return coin;
+        }
+
+        let wall1 = createWall(1000, 500, 75, 400);
+        let wall2 = createWall(700, 200, 75, 500);
+        let wall3 = createWall(1263, 262, 600, 75);
+
+        // Creating physics collisions
+        this.physics.add.collider(char1P, ground);
+        this.physics.add.collider(ground, blocks);
+        this.physics.add.collider(char1P, blocks);
+        //this.physics.add.collider(char1P, walls);
+        this.physics.add.collider(blocks, walls);
+        this.physics.add.collider(char1P, wall1, () => {
+            char1P.setVelocityY(-Math.abs(char1P.body.velocity.y) * 1.5);
+        });
+        this.physics.add.collider(char1P, wall2, () => {
+            char1P.setVelocityX(-Math.abs(char1P.body.velocity.x) + 300);
+        })
+        this.physics.add.overlap(char1P, coins, (char1P, coin) => {
+                coin.destroy();
+                this.score += 1;
+                console.log('score: ', this.score);
+                this.scoreText.setText('Score: ' + this.score);
+            }
+        );
+
+        // PUT THIS AT BOTTOM
+        this.input.on('pointermove', (pointer) => {
+            angle = Phaser.Math.Angle.BetweenPoints(char1, pointer);
+            char1.rotation = angle;
+            Phaser.Geom.Line.SetToAngle(line, char1.x, char1.y, angle, 128);
+            graphics.clear().strokeLineShape(line);
+        })
+
+        this.input.on('pointerup', () => {
+            char1P.enableBody(true, 300, 850, true, true);
+            this.physics.velocityFromRotation(angle, 1000, char1P.body.velocity);
+            this.lives -= 1;
+            this.livesText.setText('Lives: ' + this.lives);
+            if (this.lives < 0) {
+                this.scene.restart();
+            }
+        });
     }
     update() {
-
+        if(this.lives == -1) {
+            this.scene.restart();
+        }
+        if(this.score == 3) {
+            console.log("You win!");
+            this.time.delayedCall(1000, () => {
+                this.scene.start('levelthreesum');
+            })
+        }
     }
+
 }
 
 class levelTwoSummary extends Phaser.Scene{ // FINISH THIS
@@ -315,7 +437,7 @@ class levelThree extends Phaser.Scene {
     }
 }
 
-class levelThreeSummary extends Phaser.Scene { // FINISH THIS
+class levelThreeSummary extends Phaser.Scene { // FINISH THIS TOO
     constructor() {
         super('levelthreesum')
     }
@@ -339,7 +461,7 @@ const game = new Phaser.Game({
             debug: true
         },
     },
-    //scene: [levelOne, levelThree],
-    scene: [levelThree],
+    //scene: [levelOne, levelOneSummary, levelTwo, levelTwoSummary, levelThree, levelThreeSummary],
+    scene: [levelTwo],
     title: "Angry Viscous",
 });
